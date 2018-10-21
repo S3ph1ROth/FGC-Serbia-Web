49358133f981156d976698c87dd8ed3b343bacdd
S3ph1ROth <ivanjanator@gmail.com>
2018-10-21 14:50:53 +0200

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FGCSerbiaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Tournaments()
+        {
+            ViewBag.Message = "Your tournaments page.";
+
+            return View();
+        }
+        public ActionResult Twitch()
+        {
+            ViewBag.Message = "Your twitch page.";
+
+            return View();
+        }
+        public ActionResult Meetups()
+        {
+            ViewBag.Message = "Your meetups page.";
+
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FGCSerbiaWeb.Startup))]
+namespace FGCSerbiaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
